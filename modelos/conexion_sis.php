392c74faa3950c2c9c1aa02243607006edec4c41
Alex <alexbluu12@hotmail.com>
2021-10-24 17:35:02 -0500

    b

--- modelos/conexion_sis.php
+++ modelos/conexion_sis.php
@@ -3,12 +3,23 @@
 class ConexionBD{
 
     
-    Public static function cBD(){
+    function cBD(){
 
-            $bd = new PDO("mysql:host=localhost; dbname=id17821434_cormariscaldb",
-            "root","");
+        $host='localhost';
+        $dbname='BaseMoquegua';
+        $username='master';
+        $pasword='LAvidaesmrd17';
+        $puerto=1433;
 
+        try{
+            $bd = new PDO("sqlsrv:Server=$host,$puerto;Database=$dbname",$username,$pasword);
+            echo "Se conecto correctamente a la base de datos";
+        }
 
+        catch(PDOException $exp){
+            echo ("No se logro conectar con la base de datos: $dbname, error: $exp");
+        }
+        
         return $bd;
 
     }
